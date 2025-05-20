488f732b04b3893abd0ba7d6f3984a408cafcbad
Melissa Benard <benardmeli@yahoo.fr>
2025-05-20 18:01:04 +0200

    feat: get the list of the warranties

--- src/warranty/urls.py
+++ src/warranty/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from warranty.views import Home
+from warranty.views import Home, WarrantiesList
 
 # nom des urls 
 app_name = 'warranty'
 
 urlpatterns = [
   path('', Home.as_view(), name='home'), # page accueil 
-  
+  path('warranties/', WarrantiesList.as_view(), name='warranties_list'), # page liste des garanties
+
 ]
--- src/warranty/views.py
+++ src/warranty/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.conf import settings
 
+from django.views.generic import ListView
+from django.views.generic import CreateView
+from warranty.models import Warranty
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.views import View
+from django.db.models import Q
+
 # Page Accueil 
 class Home(TemplateView): 
     template_name = "warranty/home.html"
@@ -10,3 +20,12 @@ class Home(TemplateView):
         context = super().get_context_data(**kwargs)
         context['media_url'] = settings.MEDIA_URL
         return context
+
+
+# Page Liste des agents
+@method_decorator(login_required, name="dispatch")
+class WarrantiesList(ListView): 
+    model = Warranty
+    context_object_name = "warranties" # variable
+    template_name = "warranty/warranties_list.html"
+    fields = ["product_name", "brand", "purchase_date", "warranty_duration_months", "vendor", "imageReceipt", "notes", "created_at", ]
