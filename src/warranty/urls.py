9b0ddd9905f1ec8f7eb3ea12eacca18a6436a9a4
Melissa Benard <benardmeli@yahoo.fr>
2025-05-27 13:42:43 +0200

    feat: get and display the warranty details

--- src/warranty/urls.py
+++ src/warranty/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from warranty.views import Home, WarrantiesList
+from warranty.views import Home, WarrantiesList, WarrantyDetail
 
 # nom des urls 
 app_name = 'warranty'
@@ -8,5 +8,6 @@ app_name = 'warranty'
 urlpatterns = [
   path('', Home.as_view(), name='home'), # page accueil 
   path('warranties/', WarrantiesList.as_view(), name='warranties_list'), # page liste des garanties
-
+  path('warranty/<int:pk>/', WarrantyDetail.as_view(), name='warranty_detail'),
+  # page détail de la garantie
 ]
--- src/warranty/views.py
+++ src/warranty/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.views.generic import ListView
+from django.views.generic import DetailView
 from django.views.generic import CreateView
 from warranty.models import Warranty
 from django.contrib.auth.decorators import login_required
@@ -11,8 +12,9 @@ from django.shortcuts import redirect
 from django.views import View
 from django.db.models import Q
 
-# Page Accueil 
+
 class Home(TemplateView): 
+    """ Page Accueil """
     template_name = "warranty/home.html"
 
     def get_context_data(self, **kwargs):
@@ -21,10 +23,19 @@ class Home(TemplateView):
         return context
 
 
-# Page Liste des agents
 @method_decorator(login_required, name="dispatch")
 class WarrantiesList(ListView): 
+    """ Page Liste de mes garanties """
     model = Warranty
     context_object_name = "warranties" # variable
     template_name = "warranty/warranties_list.html"
+    fields = ["product_name", "brand", "purchase_date", "warranty_duration_months", "vendor", "imageReceipt", "notes", "created_at", ]
+
+
+@method_decorator(login_required, name="dispatch")
+class WarrantyDetail(DetailView):
+    """ Page Détail de la garantie """
+    model = Warranty
+    context_object_name = "warranty"
+    template_name = "warranty/warranty_detail.html"
     fields = ["product_name", "brand", "purchase_date", "warranty_duration_months", "vendor", "imageReceipt", "notes", "created_at", ]
