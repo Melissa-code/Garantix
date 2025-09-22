36f03dffacdac8842a5354bce10f6ae7405fe35b
Melissa Benard <benardmeli@yahoo.fr>
2025-09-22 17:34:53 +0200

    feat: create complete homepage layout

--- src/warranty/urls.py
+++ src/warranty/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from warranty.views import Home, WarrantiesList, WarrantyDetail, WarrantyCreate, WarrantyUpdate, WarrantyDelete
+from django.conf import settings
+from django.conf.urls.static import static
 
 # nom des urls 
 app_name = 'warranty'
@@ -14,3 +16,6 @@ urlpatterns = [
   path('create/', WarrantyCreate.as_view(), name='warranty_create'), 
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
