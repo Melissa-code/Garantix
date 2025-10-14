5b91618db9014d6de7bf3e8e0dbe50249f8d3a5a
Melissa Benard <benardmeli@yahoo.fr>
2025-10-14 17:30:14 +0200

    feat: make a strong password in create form

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -1,14 +1,30 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+import re
 
 
 class CustomUserCreationForm(UserCreationForm):
     """
-    Create 3 required fields (username = email, unique DB)
+    Create 3 required fields (username = email unique)
+    - validation password via regex 
     """
     username = forms.EmailField(label="Email", required=True)
 
     class Meta:
         model = User
         fields = ("username", "password1", "password2")
+
+    def clean_password1(self):
+        """Check complexity of password (8 char, 1 maj, 1 number, 1 symbole)"""
+        password = self.cleaned_data.get("password1")
+        pattern = r'^(?=.*[A-Z])(?=.*\d)(?=.*[-_!@#$%^&*]).+$'
+
+        if not re.match(pattern, password):
+            raise ValidationError("Le mot de passe doit contenir une majuscule, un chiffre et un symbole.")
+        
+        if len(password) < 8:
+            raise ValidationError("Le mot de passe doit contenir au moins 8 caractères.")
+        
+        return password
--- src/garantix_config/settings.py
+++ src/garantix_config/settings.py
@@ -129,6 +129,7 @@ MEDIA_ROOT = BASE_DIR / 'mediafiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/warranty/'
 
