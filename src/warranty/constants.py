63afdb9f84f1c344dc2af749fe8248908433e089
Melissa Benard <benardmeli@yahoo.fr>
2026-05-03 17:37:52 +0200

    refactor(home): create specific component card for features home

---
+++ src/warranty/constants.py
@@ -0,0 +1,14 @@
+FEATURES_HOME = [ 
+  {
+    "title": "Stockez vos garanties",
+    "description": "Tous vos documents en un seul endroit sécurisé."
+  },
+  {
+    "title": "Retrouvez facilement",
+    "description": "Recherchez vos produits rapidement et simplement."
+  },
+  {
+    "title": "Accès sécurisé",
+    "description": "Vos données sont sauvegardées en toute sécurité."
+  }
+]
--- src/warranty/views.py
+++ src/warranty/views.py
@@ -11,6 +11,7 @@ from warranty.forms import WarrantyForm
 from warranty import services
 from django.http import JsonResponse
 from django.contrib import messages
+from warranty.constants import FEATURES_HOME
 
 
 class HomeView(TemplateView): 
@@ -20,6 +21,7 @@ class HomeView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['media_url'] = settings.MEDIA_URL
+        context['features'] = FEATURES_HOME
         return context
 
 
